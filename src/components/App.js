356a47693b97931b97d9f6c03b9626b5e8a0ef93
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-07-19 16:51:18 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useReducer } from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
+const {name, color, city, bio, links} = user
+const {github, linkedin} = links
 console.log(user);
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={name} city={city} color ={color}/>
+      <About bio={bio} github={github} linkedin={linkedin}/>
     </div>
   );
 }
